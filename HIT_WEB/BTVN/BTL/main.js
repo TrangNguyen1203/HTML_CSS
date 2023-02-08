a16d4e08ec17a8ca9d2e4a3689d1d90225685bd5
unknown <nguyenthitrang.ttd@gmail.com>
2023-02-08 13:48:53 +0700

    fix

--- HIT_WEB/BTVN/BTL/main.js
+++ HIT_WEB/BTVN/BTL/main.js
@@ -14,66 +14,122 @@ window.onclick = function (event) {
         }
     }
 }
-
-$(document).ready(function(){
-    $('.img-slider').owlCarousel({
-        loop:true,
-        items:1,
-        nav:true,
-        autoplay:true,
-        autoplayTimeout:6000,
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 100) {
+            $('#travel__back').fadeIn();
+        } else {
+            $('#travel__back').fadeOut();
+        }
+    });
+    $('#travel__back').click(function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 100);
     })
-  });
+})
+
+$(document).ready(function () {
+    $('.img-slider').owlCarousel({
+        loop: true,
+        items: 1,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 8000,
+        autoplaySpeed: 2000,
+    })
+});
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.owl-product').owlCarousel({
-        loop:true,
-        margin:10,
-        autoplay:true,
-        autoplayTimeout:4000,
-        autoplaySpeed:1500,
-        autoplayHoverPause:false,
-        responsive:{
-            0 : {
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplaySpeed: 1500,
+        autoplayHoverPause: false,
+        responsive: {
+            0: {
                 items: 1
-            }, 
+            },
             481: {
                 items: 1
             },
-            767:{
-                items:2
+            767: {
+                items: 2
             },
             960: {
                 items: 3
-            } 
-        }
-    })
-  });
-  $(document).ready(function(){
-    $('.owl-staff').owlCarousel({
-        loop:true,
-        margin:10,
-        autoplay:true,
-        autoplayTimeout:4000,
-        autoplaySpeed:1500,
-        autoplayHoverPause:false,
-        responsive:{
-            0 : {
-                items: 1
-            }, 
-            481: {
-                items: 1
-            },
-            767:{
-                items:2
-            },
-            960 : {
-                items: 3
-            },
-            1169 : {
-                items : 4
             }
         }
     })
-  });
+});
+$(document).ready(function () {
+    $('.owl-staff').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplaySpeed: 1500,
+        autoplayHoverPause: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            481: {
+                items: 1
+            },
+            767: {
+                items: 2
+            },
+            960: {
+                items: 3
+            },
+            1169: {
+                items: 4
+            }
+        }
+    })
+});
+
+$(document).ready(function () {
+    $('.owl-product1').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplaySpeed: 1500,
+        autoplayHoverPause: false,
+        dots:false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            481: {
+                items: 1
+            },
+            767: {
+                items: 2
+            },
+            960: {
+                items: 3
+            },
+            1169: {
+                items: 4
+            }
+        }
+    })
+});
+
+function closeVideo(idName1)
+{
+    let id1 = document.getElementById(idName1);
+    id1.style.display = "none";
+}
+
+function display(idName)
+{
+    let id = document.getElementById(idName);
+    id.style.display = "flex";
+};
